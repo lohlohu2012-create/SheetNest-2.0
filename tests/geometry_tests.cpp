df1a60b1ea0296fd4ddf6c6b4cd32b9bbfee9b58
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-21 14:08:19 +0300

    Add 3kW Bodor cutting time estimation module

--- tests/geometry_tests.cpp
+++
@@ -1,4 +0,0 @@
-#include "sheetnest/geometry.hpp"
-#include <cassert>
-#include <cmath>
-int main(){using namespace sheetnest;Polygon p{{0,0},{10,0},{10,5},{0,5}};assert(std::abs(polygonArea(p)-50)<1e-9);assert(pointInPolygon({5,2},p));}
