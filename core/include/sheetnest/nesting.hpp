84b73d42b7213fc860862c165b677f376399565f
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-22 09:10:50 +0300

    Add production validation state to nesting API

--- core/include/sheetnest/nesting.hpp
+++ core/include/sheetnest/nesting.hpp
@@ -53,6 +53,9 @@ struct Result {
     std::vector<std::string> unplaced;
     double utilization{};
     NestingStats stats{};
+    bool productionValidated{};
+    bool productionValid{};
+    std::size_t productionIssueCount{};
 };
 
 struct NestingRunControl {
@@ -84,6 +87,7 @@ struct Options {
     double gapMm{2.0};
     std::uint32_t seed{0x534E4553u};
     bool enableOptimizer{true};
+    bool enableProductionValidation{true};
     std::shared_ptr<NestingRunControl> control{};
 };
 
