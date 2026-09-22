36a32a95296152350446558351c53ffc5dbd0992
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-22 13:25:03 +0300

    Store adaptive repair pre-round snapshots for animation

--- core/include/sheetnest/production_validation.hpp
+++ core/include/sheetnest/production_validation.hpp
@@ -45,6 +45,7 @@ struct AdaptiveRepairChange {
 
 struct AdaptiveRepairRound {
     std::size_t roundIndex{};
+    std::vector<std::vector<Placement>> beforeSheets;
     std::vector<std::vector<Placement>> afterSheets;
     std::vector<AdaptiveRepairChange> changes;
     std::vector<std::string> conflictIds;
