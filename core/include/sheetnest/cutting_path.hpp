0caae07bfba00561453b231012e00c7075627530
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-22 21:30:15 +0300

    Add inner-contour-first CAM path option

--- core/include/sheetnest/cutting_path.hpp
+++ core/include/sheetnest/cutting_path.hpp
@@ -2,4 +2,4 @@
 #include "geometry.hpp"
 #include "cutting.hpp"
 #include <vector>
-namespace sheetnest{enum class CutType{Pierce,Cut,Rapid};struct ToolMove{CutType type;Point from{},to{};double lengthMm{},speedMMin{};};struct CuttingPath{std::vector<ToolMove> moves;double totalCutLengthMm{},totalRapidLengthMm{};int pierces{};};struct PathOptions{double rapidSpeedMMin{120},pierceSeconds{.25};};CuttingPath planCuttingPath(const std::vector<Polygon>&,const CuttingParameters&,const PathOptions&);CuttingEstimate estimateCuttingPath(const CuttingPath&,const CuttingParameters&,const PathOptions&);}
+namespace sheetnest{enum class CutType{Pierce,Cut,Rapid};struct ToolMove{CutType type;Point from{},to{};double lengthMm{},speedMMin{};};struct CuttingPath{std::vector<ToolMove> moves;double totalCutLengthMm{},totalRapidLengthMm{};int pierces{};};struct PathOptions{double rapidSpeedMMin{120},pierceSeconds{.25};bool innerContoursFirst{true};};CuttingPath planCuttingPath(const std::vector<Polygon>&,const CuttingParameters&,const PathOptions&);CuttingEstimate estimateCuttingPath(const CuttingPath&,const CuttingParameters&,const PathOptions&);}
