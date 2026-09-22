b7b5e248a780b33ec8385b48593c320e6ba6852a
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-22 20:53:09 +0300

    Add nesting benchmark and NFP guard telemetry

--- core/include/sheetnest/nesting.hpp
+++ core/include/sheetnest/nesting.hpp
@@ -46,6 +46,29 @@ struct NestingStats {
     std::size_t exchangeAttempts{};
     std::size_t sheetsEliminated{};
     std::size_t optimizerPasses{};
+    std::size_t nfpTimeouts{};
+    std::size_t nfpComplexityFallbacks{};
+};
+
+struct BenchmarkMetrics {
+    double milliseconds{};
+    std::size_t placed{};
+    std::size_t skipped{};
+    std::size_t sheets{};
+    std::size_t candidateChecks{};
+    std::size_t collisionChecks{};
+    std::size_t nfpChecks{};
+    std::size_t refillMoves{};
+    std::size_t exchangeAttempts{};
+    std::size_t sheetsEliminated{};
+    std::size_t optimizerPasses{};
+    std::size_t nfpTimeouts{};
+    std::size_t nfpComplexityFallbacks{};
+};
+
+struct BenchmarkCase {
+    BenchmarkMetrics baseline{};
+    BenchmarkMetrics optimized{};
 };
 
 struct Result {
@@ -108,6 +131,12 @@ struct Options {
 };
 
 Result nest(
+    const std::vector<Instance>& instances,
+    const Sheet& sheet,
+    const Options& options
+);
+
+BenchmarkCase benchmarkNest(
     const std::vector<Instance>& instances,
     const Sheet& sheet,
     const Options& options
