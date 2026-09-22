27c669dcb6be6abf2d0cc4e82f5646d792ebab04
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-22 08:24:48 +0300

    Expose global optimizer metrics in benchmark results

--- core/include/sheetnest/benchmark.hpp
+++ core/include/sheetnest/benchmark.hpp
@@ -18,6 +18,10 @@ struct BenchmarkCase {
     std::size_t candidateChecks{};
     std::size_t collisionChecks{};
     std::size_t nfpChecks{};
+    std::size_t refillMoves{};
+    std::size_t exchangeAttempts{};
+    std::size_t sheetsEliminated{};
+    std::size_t optimizerPasses{};
 };
 
 struct BenchmarkResult {
