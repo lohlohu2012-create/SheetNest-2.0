41ed23c87e52c557f786423799f97db6c1cae913
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-23 22:23:06 +0300

    Track NFP timeout fallback in benchmark

--- core/include/sheetnest/benchmark.hpp
+++ core/include/sheetnest/benchmark.hpp
@@ -24,6 +24,7 @@ struct BenchmarkCase {
     std::size_t optimizerPasses{};
     std::size_t nfpTimeouts{};
     std::size_t nfpComplexityFallbacks{};
+    std::size_t nfpTimeoutFallbacks{};
     std::size_t nfpCacheHits{};
     std::size_t nfpCacheMisses{};
     std::vector<std::string> placedInstanceIds;
