a2e3ba3cacb0bc98273c1b3c14b09837acc103f8
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-25 13:35:18 +0300

    Add production nesting benchmark deltas and telemetry metrics

--- core/include/sheetnest/benchmark.hpp
+++ core/include/sheetnest/benchmark.hpp
@@ -15,26 +15,80 @@ struct BenchmarkCase {
     std::size_t placed{};
     std::size_t skipped{};
     double utilization{};
+
     std::size_t candidateChecks{};
     std::size_t collisionChecks{};
     std::size_t nfpChecks{};
+    std::size_t nfpAttempts{};
+    std::size_t boundsRejections{};
+    std::size_t collisionRejections{};
+    std::size_t feasibleCandidates{};
+
     std::size_t refillMoves{};
     std::size_t exchangeAttempts{};
     std::size_t sheetsEliminated{};
     std::size_t optimizerPasses{};
+
     std::size_t nfpTimeouts{};
     std::size_t nfpComplexityFallbacks{};
     std::size_t nfpTimeoutFallbacks{};
     std::size_t nfpCacheHits{};
     std::size_t nfpCacheMisses{};
+
+    std::size_t telemetryNoFeasiblePosition{};
+    std::size_t telemetryTimeouts{};
+    std::size_t telemetryInvalidGeometry{};
+    std::size_t telemetryRepairRounds{};
+    std::size_t telemetryRepairExtracted{};
+    std::size_t telemetryRepairMoved{};
+
     std::vector<std::string> placedInstanceIds;
     std::vector<std::string> skippedInstanceIds;
     std::vector<InstanceNestingTelemetry> instanceTelemetry;
 };
 
+struct BenchmarkDelta {
+    double milliseconds{};
+    std::ptrdiff_t sheets{};
+    std::ptrdiff_t placed{};
+    std::ptrdiff_t skipped{};
+    double utilizationPercentagePoints{};
+
+    std::ptrdiff_t candidateChecks{};
+    std::ptrdiff_t collisionChecks{};
+    std::ptrdiff_t nfpChecks{};
+    std::ptrdiff_t nfpAttempts{};
+    std::ptrdiff_t boundsRejections{};
+    std::ptrdiff_t collisionRejections{};
+    std::ptrdiff_t feasibleCandidates{};
+
+    std::ptrdiff_t refillMoves{};
+    std::ptrdiff_t exchangeAttempts{};
+    std::ptrdiff_t sheetsEliminated{};
+    std::ptrdiff_t optimizerPasses{};
+
+    std::ptrdiff_t nfpTimeouts{};
+    std::ptrdiff_t nfpComplexityFallbacks{};
+    std::ptrdiff_t nfpTimeoutFallbacks{};
+    std::ptrdiff_t nfpCacheHits{};
+    std::ptrdiff_t nfpCacheMisses{};
+
+    std::ptrdiff_t telemetryNoFeasiblePosition{};
+    std::ptrdiff_t telemetryTimeouts{};
+    std::ptrdiff_t telemetryInvalidGeometry{};
+    std::ptrdiff_t telemetryRepairRounds{};
+    std::ptrdiff_t telemetryRepairExtracted{};
+    std::ptrdiff_t telemetryRepairMoved{};
+
+    double elapsedImprovementPercent{};
+    double sheetReductionPercent{};
+    double utilizationImprovementPercentagePoints{};
+};
+
 struct BenchmarkResult {
     BenchmarkCase baseline;
     BenchmarkCase optimized;
+    BenchmarkDelta delta;
 };
 
 BenchmarkResult benchmarkNest(
